8c00d941a4aa74edd4f1c04927fd119c4d8afcb8
trishannaa <165927069+trishannaa@users.noreply.github.com>
2024-04-03 23:32:44 +0800

    Portfolio

---
+++ VIDAL/assets/js/main.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+AOS.init({
+
+    offset: 120,
+    delay: 0,
+    duration: 1500,
+    easing: 'ease',
+    once: false,
+    mirror: false,
+    anchorPlacement: 'top-bottom',
+});
